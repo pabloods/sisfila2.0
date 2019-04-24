70da0b05f688bc607d22aa524c997e74c2fc8c56
Pablo Oliveira <pablo_ods@hotmail.com>
2019-04-24 14:49:04 +0000

    24.04.2019,14:50

--- db/seeds.rb
+++ db/seeds.rb
@@ -181,3 +181,72 @@ Aluno.create(matricula: 213105089, nome:"Shaylon89 CCB", ativo: true, score: 9, 
 Aluno.create(matricula: 213105090, nome:"Yuri90 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
 
 
+### Criar colegiado
+col = Colegiado.create(nome: "Bacharelado em Ciência da Computação", codigo: "112140")
+
+### Atualizar colegiado dos alunos
+Aluno.where(colegiado: nil).update(colegiado: col)
+
+### Configurar título    
+Rodada.create(descricao: "Matrícula BCC - acesse http://v.ht/filabcc")
+
+### Criar usuário admin (troque a senha no comando abaixo):
+User.create(email: "admin@example.com", superadmin_role: true, password: "admin2222", password_confirmation: "admin2222")
+User.create(email: "tela@example.com", user_role: true, password: "tela2222", password_confirmation: "tela2222")
+
+### Criar mesas (você pode modificá-las em `/mesas`):
+Mesa.create(nome: "Sala 116, Mesa 1")
+Mesa.create(nome: "Sala 116, Mesa 2")
+
+### Criar grupos de alunos e filas: 
+g = Grupo.create(nome: "Prováveis concluintes")
+g.update(alunos: Aluno.where(colegiado: col).where(formando: true))
+g = Grupo.create(nome: "Alunos que realizaram pré-matrícula")
+g.update(alunos: Aluno.where(colegiado: col).where(fezPreMatricula: true))
+g = Grupo.create(nome: "Alunos com CR >= 6,0")
+g.update(alunos: Aluno.where(colegiado: col).where("score >= 60"))
+g = Grupo.create(nome: "Alunos com CR entre 4,8 e 5,9")
+g.update(alunos: Aluno.where(colegiado: col).where("score >= 48 AND score <= 59"))
+g = Grupo.create(nome: "Alunos com CR entre 3,0 e 4,7")
+g.update(alunos: Aluno.where(colegiado: col).where("score >= 30 AND score <= 47"))
+g = Grupo.create(nome: "Alunos com CR <= 2,9")
+g.update(alunos: Aluno.where(colegiado: col).where("score <= 29"))
+
+### Criar filas e associar grupos iniciais:
+fila1 = Fila.create(codigo: "CCA", nome: "Ciência da Computação, prioridade maior", posicao: 0, colegiado: col)
+fila2 = Fila.create(codigo: "CCB", nome: "Ciência da Computação, prioridade menor", posicao: 0, colegiado: col)
+g = Grupo.find_by nome: "Prováveis concluintes"
+g.update(fila: fila1)
+g = Grupo.find_by nome: "Alunos que realizaram pré-matrícula"
+g.update(fila: fila2)
+
+### Criar colegiado
+col = Colegiado.create(nome: "Bacharelado em Sistemas de Informação", codigo: "195140")
+
+### Atualizar colegiado dos alunos
+Aluno.where(colegiado: nil).update(colegiado: col)
+
+### Criar mesas (você pode modificá-las em `/mesas`):
+Mesa.create(nome: "Sala 115")
+
+### Criar grupos de alunos e filas:
+g = Grupo.create(nome: "Alunos do BSI")
+g.update(alunos: Aluno.where(colegiado: col))
+
+### Criar filas e associar grupos iniciais:
+fila = Fila.create(codigo: "BSI", nome: "Fila do BSI", posicao: 0, colegiado: col)
+g.update(fila: fila)
+
+### Criar colegiado
+col = Colegiado.create(nome: "Licenciatura em Computação", codigo: "196120")
+
+### Atualizar colegiado dos alunos
+Aluno.where(colegiado: nil).update(colegiado: col)
+
+### Criar grupos de alunos e filas:
+g = Grupo.create(nome: "Alunos de LC")
+g.update(alunos: Aluno.where(colegiado: col))
+
+### Criar filas e associar grupos iniciais:
+fila = Fila.create(codigo: "LC", nome: "Fila de LC", posicao: 0, colegiado: col)
+g.update(fila: fila)
