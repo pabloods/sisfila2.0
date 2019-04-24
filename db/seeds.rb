a0f21aaff1a917f5648c7ecf9e012f4bb8e492c2
Pablo Oliveira <pablo_ods@hotmail.com>
2019-04-24 16:33:32 +0000

    16:33

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,6 @@
 
 
 
-# Aluno.create(matricula: 213105092, nome:"Pablo", ativo: true, score: 7, fezPreMatricula: true, formando: false, colegiado_id: nil)
-
-# Aluno.create(matricula: 213105090, nome:"Ramon M", ativo: true, score: 8, fezPreMatricula: false, formando: true, colegiado_id: nil)
-
-# Aluno.create(matricula: 213105010, nome:"Zé Rafael", ativo: true, score: 10, fezPreMatricula: true, formando: true, colegiado_id: nil)
-
-# Aluno.create(matricula: 213105028, nome:"Lucas Fonseca", ativo: true, score: 5, fezPreMatricula: false, formando: false, colegiado_id: nil)
-
-# Aluno.create(matricula: 213105011, nome:"Edigar Junio", ativo: true, score: 8, fezPreMatricula: true, formando: false, colegiado_id: nil)
-
-
-
 Aluno.create(matricula: 213105001, nome:"Pablo01 SI", ativo: true, score: 7, fezPreMatricula: true, formando: false, colegiado_id: 3)
 
 Aluno.create(matricula: 213105002, nome:"Ramon02 SI", ativo: true, score: 8, fezPreMatricula: false, formando: true, colegiado_id: 3)
@@ -62,125 +50,6 @@ Aluno.create(matricula: 213105020, nome:"Paulinho20 SI", ativo: true, score: 4, 
 
 
 
-
-Aluno.create(matricula: 213105025, nome:"Lucas Fonseca25 LC", ativo: true, score: 5, fezPreMatricula: false, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105026, nome:"Jackson26 LC", ativo: true, score: 7, fezPreMatricula: false, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105027, nome:"Xandão27 LC", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 4)
-
-Aluno.create(matricula: 213105028, nome:"Ernando28 LC", ativo: true, score: 6, fezPreMatricula: true, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105029, nome:"Ignacio29 LC", ativo: true, score: 1, fezPreMatricula: false, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105030, nome:"Lucas Fonseca30 LC", ativo: true, score: 5, fezPreMatricula: false, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105031, nome:"Jackson31 LC", ativo: true, score: 7, fezPreMatricula: false, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105032, nome:"Xandão32 LC", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 4)
-
-Aluno.create(matricula: 213105033, nome:"Ernando33 LC", ativo: true, score: 6, fezPreMatricula: true, formando: false, colegiado_id: 4)
-
-Aluno.create(matricula: 213105034, nome:"Ignacio34 LC", ativo: true, score: 1, fezPreMatricula: false, formando: false, colegiado_id: 4)
-
-
-
-
-
-Aluno.create(matricula: 213105040, nome:"Douglas Augusto40 CCA", ativo: true, score: 9, fezPreMatricula: true, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105041, nome:"Elton41 CCA", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105042, nome:"Ramires42 CCA", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105043, nome:"Flavio43 CCA", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105044, nome:"Gregore44 CCA", ativo: true, score: 9, fezPreMatricula: true, formando: true, colegiado_id: 2)
-
-
-
-
-Aluno.create(matricula: 213105050, nome:"Guilherme50 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105051, nome:"Marco Antonio52 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105053, nome:"Nilton53 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105054, nome:"Shaylon54 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105055, nome:"Yuri55 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105056, nome:"Guilherme56 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105057, nome:"Marco Antonio57 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105058, nome:"Nilton58 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105059, nome:"Shaylon59 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105060, nome:"Yuri60 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105061, nome:"Guilherme61 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105062, nome:"Marco Antonio62 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105063, nome:"Nilton63 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105064, nome:"Shaylon64 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105065, nome:"Yuri65 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105066, nome:"Guilherme66 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105067, nome:"Marco Antonio67 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105068, nome:"Nilton68 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105069, nome:"Shaylon69 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105070, nome:"Yuri70 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105071, nome:"Guilherme71 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105072, nome:"Marco Antonio72 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105073, nome:"Nilton73 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105074, nome:"Shaylon74 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105075, nome:"Yuri75 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105076, nome:"Guilherme76 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105077, nome:"Marco Antonio77 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105078, nome:"Nilton78 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105079, nome:"Shaylon79 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105080, nome:"Yuri80 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105081, nome:"Guilherme81 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105082, nome:"Marco Antonio82 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105083, nome:"Nilton83 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105084, nome:"Shaylon84 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105085, nome:"Yuri85 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105086, nome:"Guilherme86 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105087, nome:"Marco Antonio87 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105088, nome:"Nilton88 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: true, colegiado_id: 2)
-
-Aluno.create(matricula: 213105089, nome:"Shaylon89 CCB", ativo: true, score: 9, fezPreMatricula: true, formando: false, colegiado_id: 2)
-
-Aluno.create(matricula: 213105090, nome:"Yuri90 CCB", ativo: true, score: 9, fezPreMatricula: false, formando: false, colegiado_id: 2)
-
-
 ### Criar colegiado
 col = Colegiado.create(nome: "Bacharelado em Ciência da Computação", codigo: "112140")
 
@@ -191,8 +60,9 @@ Aluno.where(colegiado: nil).update(colegiado: col)
 Rodada.create(descricao: "Matrícula BCC - acesse http://v.ht/filabcc")
 
 ### Criar usuário admin (troque a senha no comando abaixo):
-User.create(email: "admin@example.com", superadmin_role: true, password: "admin2222", password_confirmation: "admin2222")
-User.create(email: "tela@example.com", user_role: true, password: "tela2222", password_confirmation: "tela2222")
+User.create(email: "januario@example.com", superadmin_role: true, password: "jnr1111", password_confirmation: "jnr1111")
+User.create(email: "rodrigo@example.com", user_role: true, password: "rdrg2222", password_confirmation: "rdrg2222")
+User.create(email: "pablo@example.com", user_role: true, password: "pbl3333", password_confirmation: "pbl3333")
 
 ### Criar mesas (você pode modificá-las em `/mesas`):
 Mesa.create(nome: "Sala 116, Mesa 1")
